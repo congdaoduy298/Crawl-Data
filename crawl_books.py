af547c5a68054dd78eeffc0e25dca90cdd13aab0
Cong Dao <congdaoduy298@gmail.com>
2020-09-01 15:09:20 +0700

    Add Rate and Description columns

--- crawl_books.py
+++ crawl_books.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.keys import Keys 
 from selenium import webdriver 
-from utils import logIn, getNLID, getAuthor, nextPage
+from utils import logIn, getNLID, getAuthor, nextPage, getRate, getDescription
 from time import sleep
 import pandas as pd 
 import numpy as np 
@@ -19,19 +19,27 @@ if __name__ == "__main__":
     # 3. Login website
     logIn(browser)
 
-    # 4. Get names, authors, links, ids from web
-    names, authors, links, ids = [], [], [], []
+    # 4-1. Get names, authors, links, ids, rates from root web pages
+    names, authors, links, ids, rates, descriptions = [], [], [], [], [], []
     flag = True 
     while flag:
         getNLID(browser, names, links, ids)
         getAuthor(browser, authors)
+        getRate(browser, rates)
         flag = nextPage(browser, flag)
         # Wait browser load data 
         sleep(random.randint(3, 6))
+        break
+    
+    # 4-2. Get descriptions
+    for i in range(len(names)):
+        browser.get(links[i])
+        getDescription(browser, descriptions) 
+        sleep(random.randint(3, 5))
 
     # 5. Save data to csv
-    arr = np.asarray([ids, names, authors, links]).T
-    header = ['Id', 'Name', 'Authors', 'Link']
+    arr = np.asarray([ids, names, authors, rates, links, descriptions]).T
+    header = ['Id', 'Name', 'Authors', 'Rate', 'Link', 'Description']
     pd.DataFrame(arr).to_csv('myDatabase.csv', header=header, index=False)
     
     # 6. Close URL
